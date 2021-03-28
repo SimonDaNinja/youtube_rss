c0398af51b6f47146485be3384c6dd4b09be851a
simondaninja <simon.liljestrand@protonmail.com>
2021-03-28 20:11:32 +0200

    add copyright notice

--- youtube_rss.py
+++ youtube_rss.py
@@ -1,4 +1,26 @@
 #! /usr/bin/env python3
+
+#   SimonDaNinja/youtube_rss - a set of tools for supporting development
+#   of anonymous RSS-based YouTube client applications
+
+#   Copyright (C) 2021  Simon Liljestrand
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#   Contact by email: simon@simonssoffa.xyz
+
+
 from html.parser import HTMLParser
 import requests as req
 import re
