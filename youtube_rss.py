1f89431776bf53581c94b844c32b601fc336e373
simondaninja <simon.liljestrand@protonmail.com>
2021-03-28 17:17:39 +0200

    add support for using tor (or arbitrary function for making http requests)

--- youtube_rss.py
+++ youtube_rss.py
@@ -3,6 +3,8 @@ from html.parser import HTMLParser
 import requests as req
 import re
 import feedparser
+from tor_requests.tor_requests import getHttpResponseUsingSocks5
+import os
 
 ###########
 # classes #
@@ -71,9 +73,9 @@ def escapeQuery(query):
     return query.replace(' ', '+').replace('++','+')
 
 #use this function to get html for a youtube channel query
-def getChannelQueryHtml(query):
+def getChannelQueryHtml(query, getHttpContent = req.get):
     url = 'https://youtube.com/results?search_query=' + escapeQuery(query) + '&sp=EgIQAg%253D%253D'
-    response = req.get(url)
+    response = getHttpContent(url)
     return response.text
 
 # use this function to ask an interactive yes/no question to the user
@@ -92,8 +94,8 @@ def doYnQuery(query):
 # central functions #
 
 # if you have a channel url, you can use this function to extract the rss address
-def getRssAddressFromChannelUrl(url):
-    response = req.get(url)
+def getRssAddressFromChannelUrl(url, getHttpContent = req.get):
+    response = getHttpContent(url)
     htmlContent = response.text
     parser = RssAddressParser()
     parser.feed(htmlContent)
@@ -104,30 +106,41 @@ def getRssAddressFromChannelId(channelId):
     return f"https://www.youtube.com/feeds/videos.xml?channel_id={channelId}"
 
 # use this function to get query results from searching for a channel
-def getChannelQueryResults(query):
-    htmlContent = getChannelQueryHtml(query)
+def getChannelQueryResults(query, getHttpContent = req.get):
+    htmlContent = getChannelQueryHtml(query, getHttpContent = getHttpContent)
     parser = ChannelQueryParser()
     parser.feed(htmlContent)
     return parser.resultList
 
 # use this function to get rss entries from channel id
-def getRssEntriesFromChannelId(channelId):
+def getRssEntriesFromChannelId(channelId, getHttpContent = req.get):
     rssAdress = getRssAddressFromChannelId(channelId)
-    response = req.get(rssAdress)
+    response = getHttpContent(rssAdress)
     rssContent = response.text
     entries = feedparser.parse(rssContent)['entries']
     return entries
 
 if __name__ == '__main__':
+    useTor = doYnQuery("Do you want to use tor?")
+    if useTor:
+        getHttpContent = getHttpResponseUsingSocks5
+    else:
+        getHttpContent = req.get
     query = input("enter channel to search for: ")
     resultList = getChannelQueryResults(query)
     print(f"these channels were found by searching for '{query}':\n")
     for i, result in enumerate(resultList):
         print(f"search result {i+1}:\nTitle: {result.title}\nChannel ID: {result.channelId}\nRSS feed: {getRssAddressFromChannelId(result.channelId)}\n")
         if doYnQuery('Is this the channel whose RSS you want to parse?'):
-            entries = getRssEntriesFromChannelId(result.channelId)
+            entries = getRssEntriesFromChannelId(result.channelId, getHttpContent = getHttpContent)
             break
     
     print("\nthese videos are in the rss feed:\n")
     for entry in entries:
         print(f'video title: {entry.title}\nvideo url: {entry.link}\n')
+        if doYnQuery("Do you want to view this video?"):
+            if useTor:
+                os.system('torsocks -i mpv ' + entry.link + ' > /dev/null 2>&1 &')
+            else:
+                os.system('mpv ' + entry.link + ' > /dev/null 2>&1 &')
+            break
